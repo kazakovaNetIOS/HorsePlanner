93b19cdd0563f0d2209525046b44cd053b2164c0
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-29 01:09:03 +0300

    Replace optional params with enum with success/failure cases

--- HorsePlanner/TrainingsTodayAPI/RemoteTrainingsTodayLoader.swift
+++ HorsePlanner/TrainingsTodayAPI/RemoteTrainingsTodayLoader.swift
@@ -1,4 +1,4 @@
- //
+//
 //  RemoteTrainingsTodayLoader.swift
 //  HorsePlanner
 //
@@ -7,8 +7,13 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteTrainingsTodayLoader {
@@ -21,16 +26,17 @@ public final class RemoteTrainingsTodayLoader {
     }
     
     public init(url: URL,
-         client: HTTPClient) {
+                client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
+            case .failure:
                 completion(.connectivity)
             }
         }
