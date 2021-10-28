2579cbe45e5e41fb873bb42c1b4e3de61306c0e6
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-29 00:06:13 +0300

    Guarantee that we can load more than once

--- HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
+++ HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
@@ -24,6 +24,16 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "http://a-givenw-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://a-givenw-url.com")!)
@@ -36,9 +46,11 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
