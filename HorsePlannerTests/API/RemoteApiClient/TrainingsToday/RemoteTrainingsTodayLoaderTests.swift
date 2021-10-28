30dbcc90ca4891ba10a6f641b1196673edcdec45
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-29 00:23:21 +0300

    Delivers connectivity error on client error

--- HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
+++ HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
@@ -34,6 +34,17 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnCLientsError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteTrainingsTodayLoader.Error?
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://a-givenw-url.com")!)
@@ -46,9 +57,13 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
+            requestedURLs.append(url)            
         }
     }
 }
