43448c0f790a2ff34fdeeb9f581f50d1360281a8
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-28 23:17:46 +0300

    Refactor test to remove duplication

--- HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
+++ HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
@@ -37,32 +37,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-     func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteTrainingsTodayLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "http://a-givenw-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteTrainingsTodayLoader(url: url, client: client)
-        
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-givenw-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteTrainingsTodayLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
-
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "http://a-givenw-url.com")!)
+    ->    (sut: RemoteTrainingsTodayLoader,
+           client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteTrainingsTodayLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
 }
