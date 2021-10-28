7b0dee09936383d6e00f06c08e164329385cc05e
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-28 22:40:09 +0300

    Request data from url on load

--- HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
+++ HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
@@ -19,20 +19,34 @@ import XCTest
 //}
 
 class RemoteTrainingsTodayLoader{
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteTrainingsTodayLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteTrainingsTodayLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL(){
+        let client = HTTPClient.shared
+        let sut = RemoteTrainingsTodayLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
