c11b32b8e9edbd9136366e667c8bcca1904d0084
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-29 23:55:41 +0300

    Refactor helper to receive Result instead of Error types to enable reusability for different potential results

--- HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
+++ HorsePlannerTests/API/RemoteApiClient/TrainingsToday/RemoteTrainingsTodayLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     func test_load_deliversErrorOnCLientsError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWithError: .connectivity, when: {
+        expect(sut, toCompleteWith: .failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
@@ -49,7 +49,7 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
         
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithError: .invalidData, when: {
+            expect(sut, toCompleteWith: .failure(.invalidData), when: {
                 client.complete(withStatusCode: code, at: index)
             })
         }
@@ -58,7 +58,7 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     func test_load_deloversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWithError: .invalidData, when: {
+        expect(sut, toCompleteWith: .failure(.invalidData), when: {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
@@ -66,14 +66,11 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
-
-        var capturedResults = [RemoteTrainingsTodayLoader.Result]()
-        sut.load { capturedResults.append($0) }
         
-        let emptyListJSON = Data("{\"items\": []}".utf8)
-        client.complete(withStatusCode: 200, data: emptyListJSON)
-        
-        XCTAssertEqual(capturedResults, [.success([])])
+        expect(sut, toCompleteWith: .success([]), when: {
+            let emptyListJSON = Data("{\"items\": []}".utf8)
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        })
     }
     
     // MARK: - Helpers
@@ -87,7 +84,7 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: RemoteTrainingsTodayLoader,
-                        toCompleteWithError error: RemoteTrainingsTodayLoader.Error,
+                        toCompleteWith result: RemoteTrainingsTodayLoader.Result,
                         when action: () -> Void,
                         file: StaticString = #filePath,
                         line: UInt = #line) {
@@ -96,7 +93,7 @@ class RemoteTrainingsTodayLoaderTests: XCTestCase {
         
         action()
         
-        XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
+        XCTAssertEqual(capturedResults, [result], file: file, line: line)
     }
     
     private class HTTPClientSpy: HTTPClient {
