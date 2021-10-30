07dadc4dea934d59e5354742b3a7502c24d6e762
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-30 12:40:33 +0300

    Move TrainingsTodayMapper to its own file

--- HorsePlanner/TrainingsTodayAPI/RemoteTrainingsTodayLoader.swift
+++ HorsePlanner/TrainingsTodayAPI/RemoteTrainingsTodayLoader.swift
@@ -43,36 +43,3 @@ public final class RemoteTrainingsTodayLoader {
         }
     }
 }
-
-private class TrainingsTodayMapper {
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let horseName: String
-        let date: Date?
-        let location: String
-        
-        var item: TrainingsTodayItem {
-            TrainingsTodayItem(id: id,
-                               horseName: horseName,
-                               date: date,
-                               location: location)
-        }
-    }
-    
-    static var OK_200: Int { 200 }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [TrainingsTodayItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteTrainingsTodayLoader.Error.invalidData
-        }
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(DateFormatter.full)
-        
-        let root = try decoder.decode(Root.self, from: data)
-        return root.items.map { $0.item }
-    }
-}
